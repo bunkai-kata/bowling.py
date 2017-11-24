aad1d8334849ceb99e6e8a3f1dc2d701b12ee683
Geoff Shannon <geoffpshannon@gmail.com>
2017-11-24 00:33:25 -0800

    9

--- bowling.py
+++ bowling.py
@@ -10,6 +10,8 @@ class Game:
 
     def score(self):
         score = 0
-        for i in range(len(self._rolls)):
-            score += self._rolls[i]
+        i = 0
+        for i in range(10):
+            score += self._rolls[i] + self._rolls[i+1]
+            i += 2
         return score
