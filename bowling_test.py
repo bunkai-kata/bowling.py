5ed96942e8f504649eb79db14c7261e0cd395d7e
Geoff Shannon <geoffpshannon@gmail.com>
2017-11-24 00:07:21 -0800

    3

--- bowling_test.py
+++ bowling_test.py
@@ -1,16 +1,17 @@
+import pytest
 from bowling import *
 
-def test_gutter_game():
-    g = Game()
+@pytest.fixture
+def g():
+    return Game()
 
+def test_gutter_game(g):
     for i in range(20):
         g.roll(0)
 
     assert 0 == g.score()
 
-def test_all_ones():
-    g = Game()
-
+def test_all_ones(g):
     for i in range(20):
         g.roll(1)
 
