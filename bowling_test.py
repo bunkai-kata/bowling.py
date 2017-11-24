75c8a912297d0478709f6a4c35c78b56c835be5d
Geoff Shannon <geoffpshannon@gmail.com>
2017-11-24 00:15:58 -0800

    4

--- bowling_test.py
+++ bowling_test.py
@@ -17,9 +17,9 @@ def test_all_ones(g):
     roll_many(g, 20, 1)
     assert 20 == g.score()
 
-# def test_one_spare(g):
-#     g.roll(5)
-#     g.roll(5) # spare
-#     g.roll(3)
-#     roll_many(g, 17, 0)
-#     assert 16 == g.score()
+def test_one_spare(g):
+    g.roll(5)
+    g.roll(5) # spare
+    g.roll(3)
+    roll_many(g, 17, 0)
+    assert 16 == g.score()
