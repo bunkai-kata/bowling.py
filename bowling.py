b73736adbcea4029cf4a06f4b5e65da79c45d138
Geoff Shannon <geoffpshannon@gmail.com>
2017-11-24 00:15:31 -0800

    3

--- bowling.py
+++ bowling.py
@@ -2,9 +2,14 @@ class Game:
 
     def __init__(self):
         self._score = 0
+        self._rolls = []
 
     def roll(self, pins):
         self._score += pins
+        self._rolls.append(pins)
 
     def score(self):
+        score = 0
+        for i in range(len(self._rolls)):
+            score += self._rolls[i]
         return self._score
